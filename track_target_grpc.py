e4a40874739a57fce189e5838928f11f871fd1a6
ee597 <shaikbep@usc.edu>
2020-11-24 16:29:33 -0800

    added methods for three types of packets and other codes changes untested

--- track_target_grpc.py
+++ track_target_grpc.py
@@ -19,7 +19,7 @@ import xmlrpc.client
 
 uavs = []
 # live list of all nodes: 0 -- dead; 1 - alive
-live_list = {}
+ack_list = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0 }
 intention_list = {}
 # trackedList maintains which target is maintained by whom
 trackedList = {}
@@ -49,6 +49,7 @@ class CORENode():
     self.trackid = track_nodeid
     self.oldtrackid = track_nodeid
     self.last_seen = None
+    self.cur_intention = -1
 
   def __repr__(self):
     return str(self.nodeid)
@@ -63,7 +64,10 @@ class ReceiveUDPThread(threading.Thread):
     
   def run(self):
     ReceiveUDP()
-      
+
+# reseting ack list to 0's for all nodes
+def AckList_reset():
+  ack_list = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0} 
 
 #---------------
 # Calculate the distance between two modes (on a map)
@@ -91,16 +95,16 @@ def RecordTarget(uavnode):
 #---------------
 # Advertise the target being tracked over UDP
 #---------------
-def AdvertiseUDP(uavnodeid, trgtnodeid, intention_flag):
+def AdvertiseUDP(buf):
   print("AdvertiseUDP")
   addrinfo = socket.getaddrinfo(mcastaddr, None)[0]
   sk = socket.socket(addrinfo[0], socket.SOCK_DGRAM)
   ttl_bin = struct.pack('@i', ttl)
   sk.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl_bin)
-  buf = str(intention_flag) + ' ' + str(uavnodeid) + ' ' + str(trgtnodeid)
+  #buf = str(intention_flag) + ' ' + str(uavnodeid) + ' ' + str(trgtnodeid)
   sk.sendto(buf.encode(encoding='utf-8',errors='strict'), (addrinfo[4][0], port))
   print("timestamp", datetime.datetime.now())
-  print("I'm advertising this", str(intention_flag) + ' ' + str(uavnodeid) + ' ' + str(trgtnodeid))
+  print("I'm advertising this", buf)
 
 #---------------
 # Receive and parse UDP advertisments
@@ -123,17 +127,59 @@ def ReceiveUDP():
     print("ReceiveUDP")
     buf, sender = sk.recvfrom(1500)
     buf_str = buf.decode('utf-8')
-    trgtidstr, intention_str, uavidstr,  = buf_str.split(" ")        
-    intention_flag, uavnodeid, trgtnodeid = int(intention_str), int(uavidstr), int(trgtidstr)
-    print("timestamp", datetime.datetime.now())
-    print("intention_flag, uavnodeid, trgtnodeid", intention_str, uavidstr, trgtidstr)
-    if intention_flag:	
-      intention_list[uavnodeid] = trgtnodeid
-    # Update tracking info for other UAVs
-    uavnode = uavs[mynodeseq]
-    if uavnode.nodeid != uavnodeid:
+    myuavnode = uavs[mynodeseq]
+    packet = buf_str.split(" ")
+    if packet[0] == "ack":
+      updateAck_list(packet, myuavnode)
+      
+    elif packet[0] == "in":
+      sendAckPacket(packet, myuavnode)
+      #trgtidstr, intention_str, uavidstr  = buf_str.split(" ")        
+      #intention_flag, uavnodeid, trgtnodeid =  int(packet[1]), int(packet[2])
+      #print("timestamp", datetime.datetime.now())
+      #print("intention_flag, uavnodeid, trgtnodeid", intention_str, uavidstr, trgtidstr)
+      # its a contention packet I need to sent an ack back
+      #if intention_flag:	
+      #intention_list[uavnodeid] = trgtnodeid
+    #its a normal status pack
+    else:
+      uavnodeid, trgtnodeid = packet[0], packet[1]
       UpdateTracking(uavnodeid, trgtnodeid)
+    #trgtidstr, intention_str, uavidstr  = buf_str.split(" ")        
+    #intention_flag, uavnodeid, trgtnodeid = int(intention_str), int(uavidstr), int(trgtidstr)
+    #print("timestamp", datetime.datetime.now())
+    #print("intention_flag, uavnodeid, trgtnodeid", intention_str, uavidstr, trgtidstr)
+    # its a contention packet I need to sent an ack back
+    #if intention_flag:	
+    #  intention_list[uavnodeid] = trgtnodeid
+      
+    # Update tracking info for other UAVs
+    #myuavnode = uavs[mynodeseq]
+    #if myuavnode.nodeid != uavnodeid:
+    #  UpdateTracking(uavnodeid, trgtnodeid)
+
+def updateAck_list(packet, myuavnode):
+  senderUAV = int(packet[1])
+  receiverUAV = int(packet[2])
+  trgtnodeid = int(packet[3])
+  if receiverUAV == myuavnode.nodeid and trgtnodeid == myuavnode.cur_intention:
+    ack_list[senderUAV] = 1
+
+#make an acket pack
+def createAckPacket(senderUAVid, receiverUAVid, targetid):
+  buf = 'ack' + ' ' + str(senderUAVid) + ' ' + str(receiverUAVid) + ' ' + str(targetid)
+  return buf
   
+def sendAckPacket(packet, myuavnode):
+  # its a contention packet I need to sent an ack back
+  #trgtidstr, intention_str, uavidstr  = buf_str.split(" ") 
+  uavnodeid, trgtnodeid =  int(packet[1]), int(packet[2])
+  buf = createAckPacket(myuavnode.nodeid, uavnodeid, trgtnodeid)
+  #todo: send it
+  print("timestamp", datetime.datetime.now())
+  print("intention_flag, uavnodeid, trgtnodeid", intention_str, uavidstr, trgtidstr)
+  intention_list[uavnodeid] = trgtnodeid
+    
 #---------------
 # Update tracking info based on a received advertisement
 #---------------
@@ -161,23 +207,57 @@ def UpdateTracking(uavnodeid, trgtnodeid):
   if protocol == "udp":
     thrdlock.release()
 
+def CreateIntentionPacket(uavnodeid, trgtnodeid):
+  return 'in' + ' ' + str(uavnodeid) + ' ' + str(trgtnodeid)
+  
 #to consult with other nodes and make a decision
 def Mutual_Consultation(uavnode, trgtnodeid):
   print("Mutual_Consultation")
 	#create a intention message and send it, just make one more entry into the same function as intention flag
   #intention_list.clear();	
-  AdvertiseUDP(1, uavnode.nodeid, trgtnodeid)
+  # I'm sending a intention message I want to make sure all the live nodes got it
+  # by getting back their acks
+  # reset the acklist for this particular target
+  AckList_reset()
+  #AdvertiseUDP(1, uavnode.nodeid, trgtnodeid)
   #AdvertiseUDP(1, uavnode.nodeid, uavnode.trackid)
   #AdvertiseUDP(1, uavnode.nodeid, uavnode.trackid)
   #AdvertiseUDP(1, uavnode.nodeid, uavnode.trackid)
 #go for sleep for 1/2 of second till everyone sends their opinion
-  print("timestamp", datetime.datetime.now())
-  print("Going for sleep now")
-  thrdlock.release()
-  time.sleep(2)
-  print("timestamp", datetime.datetime.now())
-  print("Just wokeup from sleep now")
-  thrdlock.acquire()
+  #print("timestamp", datetime.datetime.now())
+  #print("Going for sleep now")
+  #thrdlock.release()
+  #time.sleep(2)
+  #print("timestamp", datetime.datetime.now())
+  #print("Just wokeup from sleep now")
+  #thrdlock.acquire()
+  # I should get data from all the live nodes, else retransmit
+  AllgotPacket = False
+  while not AllgotPacket:
+    for uavnodetmp in uavs:
+    #make decision if he is alive
+      if uavnodetmp.last_seen - time.time() <= 3:
+      #so he is alive, check if you have got an ack from him
+      #you did not get a ack from him, retransmit
+        if ack_list[uavnodetmp.nodeid] == 0:
+          uavnode.cur_intention = trgtnodeid
+          buf = CreateIntentionPacket(uavnode.nodeid, trgtnodeid)
+          AdvertiseUDP(buf)
+  #AdvertiseUDP(1, uavnode.nodeid, uavnode.trackid)
+  #AdvertiseUDP(1, uavnode.nodeid, uavnode.trackid)
+  #AdvertiseUDP(1, uavnode.nodeid, uavnode.trackid)
+  #go for sleep for 1/2 of second till everyone sends their opinion
+          print("timestamp", datetime.datetime.now())
+          print("Going for sleep now")  
+          thrdlock.release()
+          time.sleep(2)
+          print("timestamp", datetime.datetime.now())
+          print("Just wokeup from sleep now")
+          thrdlock.acquire()
+          break
+    else:
+      AllgotPakcet = True
+  #todo: I also need to make sure that I received everyone's constent   
   track_flag = 1
   for node_id, target_id in intention_list.items():
     if target_id == trgtnodeid and uavnode.nodeid > node_id:			
@@ -218,6 +298,7 @@ def TrackTargets(covered_zone, track_range):
         print("Already being tracked by someone, no need to track it")
         trackflag = 1
       if curTime - lastEntry >= expirationTimer:  
+        print("remove target and node entry from tagets track dict")
         trackedList.pop(trgtnode_id)
         
     # If this UAV was tracking this target before and it's still
@@ -270,8 +351,10 @@ def TrackTargets(covered_zone, track_range):
           
   # Advertise target being tracked if using comms 
   if protocol == "udp":
-    AdvertiseUDP(0, uavnode.nodeid, uavnode.trackid)
-    AdvertiseUDP(0, uavnode.nodeid, uavnode.trackid)
+    #send a status packet
+    buf = CreateStatusPacket(uavnode.nodeid, uavnode.trackid)   
+    AdvertiseUDP(buf)
+    #AdvertiseUDP(0, uavnode.nodeid, uavnode.trackid)
     #AdvertiseUDP(0, uavnode.nodeid, uavnode.trackid)
     #AdvertiseUDP(0, uavnode.nodeid, uavnode.trackid) 	
     
@@ -282,7 +365,9 @@ def TrackTargets(covered_zone, track_range):
     RecordTarget(uavnode)
     if uavnode.trackid == -1:
       RedeployUAV(uavnode)
-  
+
+def CreateStatusPacket(uavnodeid, trackid):
+  return str(uavnodeid) + ' ' + str(trackid)
   
 #---------------
 # main
